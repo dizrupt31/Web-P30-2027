9373ee53037359960393683b1d4dc34a223c59e6
st403 <st403@WS403-11>
2026-03-30 09:19:42 +0300

    Добавил папку модел,класс дайвера и базовый абстрактый класс

---
+++ Dive_Center/Models/Diver.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dive_Center.Models
+{
+    public class Diver : EFModel
+    {
+        [Required(ErrorMessage = "Email обязателен")]
+        [EmailAddress(ErrorMessage = "Некорректный email")]
+        public string? Email { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string? CertificationLevel { get; set; }
+    }
+}
